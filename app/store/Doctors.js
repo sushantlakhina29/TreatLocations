5ce406c52125951ee3f5558676377bd93bab2ed6
sushantlakhina29 <sushatlakhina29@gmail.com>
2014-04-22 02:03:15 +0530

    Delete Doctors.js

--- app/store/Doctors.js
+++
@@ -1,9 +0,0 @@
-Ext.define('TreatLocations.store.Doctors', {
-    extend : "Ext.data.Store",
-    requires : [ 'Location.model.Doctors' ],
-    config : {
-        storeId : 'providerStore',
-        model : 'Location.model.Doctors',
-	autoLoad: true
-    }
-});
